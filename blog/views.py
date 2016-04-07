ec7aa9b8251e095d80e46c6b8dd210ae8f867b6c
Bruno <fsouza.bruno@gmail.com>
2016-04-07 14:12:36 -0300

    added django forms!

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+
 
 # return list of posts ordered by published date
 def post_list(request):
@@ -9,7 +12,37 @@ def post_list(request):
 
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
 # get details for individual posts or 404 page
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})

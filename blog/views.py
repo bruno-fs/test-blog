e7a23b819b76f7835a3d94f96227b81d6c553655
Bruno <fsouza.bruno@gmail.com>
2016-04-08 18:09:17 -0300

    added a view for unpublished posts and started to require login (not done yet)

--- blog/views.py
+++ blog/views.py
@@ -2,30 +2,34 @@ from django.shortcuts import redirect
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 from .models import Post
 from .forms import PostForm
 
 
-# return list of posts ordered by published date
+# list of posts ordered by published date
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
-# get details for individual posts or 404 page
+# details for individual posts or 404 page
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
+# create new post
+@login_required()
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -33,6 +37,8 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
+# edit posts
+@login_required()
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -40,9 +46,48 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+# publish drafts
+@login_required()
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
+
+
+# unpublished posts
+@login_required()
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+# remove post
+@login_required()
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
+
+# login
+# def login_view(request):
+#     username = request.POST['username']
+#     password = request.POST['password']
+#     user = authenticate(username=username, password=password)
+#     if user is not None:
+#         if user.is_active:
+#             login(request, user)
+#             # Redirect to a success page.
+#         else:
+#             # Return a 'disabled account' error message
+#
+#     else:
+#
+#     # Return an 'invalid login' error message.
--- mysite/settings.py
+++ mysite/settings.py
@@ -123,3 +123,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# url for login
+LOGIN_REDIRECT_URL = '/'

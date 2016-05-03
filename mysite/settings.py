556d7247ec644beb736a2b8e9307a90c522c184b
Bruno FS <fsouza.bruno@gmail.com>
2016-05-03 11:17:34 -0300

    moved templates to root

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,4 +125,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # url for login
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'blog:post_list'
